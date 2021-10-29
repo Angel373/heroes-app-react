6568a8934130e15b5431560b3a45076954d1de8e
Jhon <angeljhonvegachampi@gmil.com>
2021-10-28 21:57:06 -0500

    login works

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -1,60 +1,63 @@
-import React from 'react'
-import { Link, NavLink } from 'react-router-dom'
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import { useContext } from "react/cjs/react.development";
+import { AuthContext } from "../../auth/AuthContext";
 
 export const Navbar = () => {
+
+    const {user: {name}} = useContext(AuthContext)
+    console.log(name);
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
-            
-            <Link 
-                className="navbar-brand" 
-                to="/"
-            >
-                Asociaciones
+        <div className="container-fluid">
+            <Link className="navbar-brand" to="/">
+            Asociaciones
             </Link>
 
             <div className="navbar-collapse">
-                <div className="navbar-nav">
+            <div className="navbar-nav">
+                <NavLink
+                activeClassName="active"
+                className="nav-item nav-link"
+                exact
+                to="/marvel"
+                >
+                Marvel
+                </NavLink>
 
-                    <NavLink 
-                        activeClassName="active"
-                        className="nav-item nav-link" 
-                        exact
-                        to="/marvel"
-                    >
-                        Marvel
-                    </NavLink>
-
-                    <NavLink 
-                        activeClassName="active"
-                        className="nav-item nav-link" 
-                        exact
-                        to="/dc"
-                    >
-                        DC
-                    </NavLink>
-                    <NavLink 
-                        activeClassName="active"
-                        className="nav-item nav-link" 
-                        exact
-                        to="/search"
-                    >
-                        Search 
-                    </NavLink>
-                </div>
+                <NavLink
+                activeClassName="active"
+                className="nav-item nav-link"
+                exact
+                to="/dc"
+                >
+                DC
+                </NavLink>
+                <NavLink
+                activeClassName="active"
+                className="nav-item nav-link"
+                exact
+                to="/search"
+                >
+                Search
+                </NavLink>
+            </div>
             </div>
 
-            <div className="navbar-collapse collapse w-100 order-3 dual-collapse2">
-                <ul className="navbar-nav ml-auto">
-                    <NavLink 
-                        activeClassName="active"
-                        className="nav-item nav-link" 
-                        exact
-                        to="/login"
-                    >
-                        Logout
-                    </NavLink>
-                </ul>
+            <div className="d-flex">
+            <ul className="navbar-nav ml-auto">
+                <span className="nav-item nav-link text-info">{name}</span>
+                <NavLink
+                activeClassName="active"
+                className="nav-item nav-link"
+                exact
+                to="/login"
+                >
+                Logout
+                </NavLink>
+            </ul>
             </div>
+        </div>
         </nav>
-    )
-}
+    );
+};
